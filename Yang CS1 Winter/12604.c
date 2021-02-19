bcb8026f671a986a680dc3faa2b22f8ee5b6ff7b
ChristianLin0420 <crlc112358@gmail.com>
2021-02-19 23:13:55 +0800

    upload new problem

--- Yang CS1 Winter/12604.c
+++ Yang CS1 Winter/12604.c
@@ -18,31 +18,31 @@ int main() {
 
 
 int init_board(int size) {
-    for(int i=0; i<size; i++)
-        for(int j=0; j<size; j++)
+    for(int i = 0; i < size; i++)
+        for(int j = 0; j < size; j++)
             board[i][j] = '0';
 }
 
-int check_queen(int r, int c, int size){
-    for(int i=1; i<=r; i++){
+int check_queen(int r, int c, int size) {
+    for(int i = 1; i <= r; i++) {
         // check_diagonal
-        if(c-i >= 0 && (board[r-i][c-i] == 'Q' || board[r-i][c-i] == 'R'))
+        if(c - i >= 0 && (board[r - i][c - i] == 'Q' || board[r - i][c - i] == 'R'))
             return 0;
-        if(c+i < size && (board[r-i][c+i] == 'Q' || board[r-i][c+i] == 'R'))
+        if(c + i < size && (board[r - i][c + i] == 'Q' || board[r - i][c + i] == 'R'))
             return 0;
-        if(board[r-i][c] == 'Q' || board[r-i][c] == 'R')
+        if(board[r - i][c] == 'Q' || board[r - i][c] == 'R')
             return 0;
     }
     return 1;
 }
 
-int check_rooks(int r, int c, int size){
-    for(int i=1; i<=r; i++){
-        if(c-i >= 0 && board[r-i][c-i] == 'Q')
+int check_rooks(int r, int c, int size) {
+    for(int i = 1; i <= r; i++) {
+        if(c - i >= 0 && board[r - i][c - i] == 'Q')
             return 0;
-        if(c+i < size && board[r-i][c+i] == 'Q')
+        if(c + i < size && board[r - i][c + i] == 'Q')
             return 0;
-        if(board[r-i][c] == 'Q' || board[r-i][c] == 'R')
+        if(board[r - i][c] == 'Q' || board[r - i][c] == 'R')
             return 0;
     }
     return 1;
